a3c8b9d729ea93fa234e816c7041e4cfa248bb7a
santosh <santoshsjiddimani1011@gmail.com>
2023-03-03 17:15:18 +0530

    one

---
+++ src/github1/push.java
@@ -0,0 +1,34 @@
+package github1;
+
+
+public class push {
+
+	public void TC_001() {
+    	System.out.println("TC_001");
+    }
+    
+    
+    public void TC_002() {
+    	System.out.println("TC_002");
+    }
+    
+   
+    public void TC_003() {
+    	System.out.println("TC_003");
+    }
+   
+    public void TC_004() {
+    	System.out.println("TC_004");
+    }
+  
+    public void TC_005() {
+    	System.out.println("TC_005");
+    }
+	public static void main(String[] args) {
+		 
+       
+        
+
+	}
+
+}
